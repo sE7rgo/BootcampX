ad62ac4a163a1df4c15ac01b563ab35279c7fd30
sE7rgo <se7rgo@gmail.com>
2020-07-22 00:29:44 +0000

    nested select queries

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -8,9 +8,9 @@ CREATE TABLE teachers (
 
 CREATE TABLE assistance_requests (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  student_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
-  teacher_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
   created_at TIMESTAMP,
   started_at TIMESTAMP,
   completed_at TIMESTAMP,
